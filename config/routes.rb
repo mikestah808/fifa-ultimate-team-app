4767fb8ec828f389d4cae81a3111635999293b2c
Michael Stafford <michaelstafford808@gmail.com>
2022-12-28 17:57:10 -1000

    removed all methods within controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  # post "/login", to: "sessions#create"
-  # delete "/logout", to: "sessions#destroy"
-  post "/signup", to: "users#create"
-  get "/me", to: "users#show"
+# post "/login", to: "sessions#create"
+#   delete "/logout", to: "sessions#destroy"
+#   post "/signup", to: "users#create"
+#   get "/me", to: "users#show"
 
 
   resources :countries
   resources :players
   resources :teams
+  resources :users
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
