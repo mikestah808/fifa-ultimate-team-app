6648480746d0cc35736b1bb93a01bab33c894952
Michael Stafford <michaelstafford808@gmail.com>
2022-12-28 17:42:47 -1000

    added routes to routes.rb and created methods for User controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorized
+
+
+  private
+
+  def authorized
+      return render json:{error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+  end
+
 end
