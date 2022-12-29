58cedc8549d366202ff77a318abe902d3edc36d6
Michael Stafford <michaelstafford808@gmail.com>
2022-12-28 19:05:12 -1000

    deleted methods from users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-# post "/login", to: "sessions#create"
-#   delete "/logout", to: "sessions#destroy"
-#   post "/signup", to: "users#create"
-#   get "/me", to: "users#show"
-
 
   resources :countries
   resources :players
