8aea02e159df0269fd5aaca73d64b4f9d81b7075
Michael Stafford <michaelstafford808@gmail.com>
2022-12-30 10:16:43 -1000

    npm installed react-router-dom version 6. Added client-side routing to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './HomePage'
+import Signup from './authentication/Signup'
+import Login from './authentication/Login'
+import Countries from './Countries'
+import Teams from './Teams'
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Hello World</p>
+        <Routes>
+          <Route exact path="/" element={ <HomePage /> } />
+          <Route exact path="/login" element={ <Login /> } />
+          <Route exact path="/signup" element={ <Signup /> } />
+          <Route exact path="/countries" element={ <Countries /> } />
+          <Route exact path="/teams" element={ <Teams /> } />
+        </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
