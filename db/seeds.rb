2057cc2e8fdc3eba9d9a28ef114144a9dd3edb32
Michael Stafford <michaelstafford808@gmail.com>
2022-12-29 16:23:12 -1000

    seeded data for user, team, country, and player models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# User.create(email: "michaelstafford808@gmail.com", password: "testpassword", first_name: "michael", last_name: "stafford")
+
+# Team.create(name: "World 11", user_id: 1)
+# Team.create(name: "OP Team", user_id: 1)
+
+# Country.create(name: "Argentina", image_url: "https://wallpaperaccess.com/full/1410676.jpg")
+# Country.create(name: "France", image_url: "https://cdn.britannica.com/82/682-004-F0B47FCB/Flag-France.jpg")
+# Country.create(name: "Brazil", image_url: "https://cdn.britannica.com/47/6847-004-7D668BB0/Flag-Brazil.jpg")
+# Country.create(name: "Germany", image_url: "https://cdn.britannica.com/97/897-004-232BDF01/Flag-Germany.jpg")
+
+
+# Player.create(name: "Lionel Messi", age: 35, image_url: "https://img.a.transfermarkt.technology/portrait/big/28003-1671435885.jpg?lm=1", position: "FWD", rating: 94, club: "Paris Saint Germain", price: 50000000, pace: 95, dribbling: 99, shooting: 92, defending: 50, passing: 95, physical: 40, team_id: 1, country_id: 1)
+
+
+# AFTER WORKOUT AND EATING, SEED PLAYER DATA AND COUNTRY DATA AND SEE IF ASSOCIATIONS WORK PROPERLY IN RAILS CONSOLE
+# MAKE SURE TO COMMENT OUT SEEDED DATA AFTER USING RAILS DB: SEED!!!
