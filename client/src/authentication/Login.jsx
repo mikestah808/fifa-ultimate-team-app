e00f87154f7363920bca09ac46a033b470779b5e
Michael Stafford <michaelstafford808@gmail.com>
2023-01-05 15:31:33 -1000

    installed material ui version 5.11.3

--- client/src/authentication/Login.jsx
+++ client/src/authentication/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+// import React, { useState } from 'react'
+// import { Button } from '@mui/material'
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+
+
+
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    
+
+//     const [email, setEmail] = useState("")
+//     const [password, setPassword] = useState("")
+//     const [error, setError] = useState("")
+
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault()
+//         fetch("/login", {
+//           method: "POST",
+//           headers: {"Content-Type": "application/json"},
+//           body: JSON.stringify({
+//             email: email,
+//             password: password
+//           })
+//         })
+//         .then((res) => res.json())
+//         .then(user => {
+//           if (!user.error) {
+//             login(user)
+//             console.log("you have successfuly logged in")
+//           } else {
+//             setEmail("")
+//             setPassword("")
+//             console.log("invalid email or password")
+//             setError("Invalid Email or Password")
+//           }
+//         })
+//     }
+
+//     return (
+//       <>
+//       <h1>Login Here!</h1>
+//       <Box
+//         onSubmit={handleSubmit}
+//         component="form"
+//         sx={{
+//           '& > :not(style)': { m: 1, width: '25ch' },
+//         }}
+//         noValidate
+//         autoComplete="off"
+//       >
+//         <br />
+//         <TextField type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} label="Email" variant="outlined" />
+//         <br />
+//         <TextField type="password" id="email" value={password} onChange={(e) => setPassword(e.target.value)} label="Password" variant="outlined" />
+//         <br />
+//         <Button type='submit' variant="outlined">Login</Button>
+//       </Box>
+//       <h4>
+//         {error}
+//       </h4>
+//       </>
+//     );
+
+
 }
 
-export default Login
+
+export default Login;
