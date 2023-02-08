5602e5a077411b9ee484a383d79c85d2f7e3f25b
Michael Stafford <michaelstafford808@gmail.com>
2023-02-07 18:47:26 -1000

    customized TextField components for Login and Sign Up page

--- client/src/authentication/Login.jsx
+++ client/src/authentication/Login.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { login } from '../features/users/usersSlice';
 
+import { Box } from '@mui/system';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material'
+
 
 function Login() {
   let navigate = useNavigate();
@@ -35,24 +39,65 @@ function Login() {
     }
 
     return (
-      <>
+      <div className='login'>
       <h1>Login Here!</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-         Email:
-            <input type="text" onChange={(event) => setEmail(event.target.value)} value={email}/>
-        </label>
-        <label>
-         Password:
-           <input type="password" onChange={(event) => setPassword(event.target.value)} value={password}/>
-        </label>
-        <button type='submit'>Login</button>
-      </form>
+      <br />
+      <br />
+      <Box
+        onSubmit={handleSubmit}
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <br />
+        <TextField type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} label="Email" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+        <br />
+        <TextField type="password" id="email" value={password} onChange={(e) => setPassword(e.target.value)} label="Password" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+        <br />
+        <Button sx={{ color: 'white' }} type='submit' variant="outlined">Login</Button>
+      </Box>
       <h4>
         {error}
       </h4>
-      </>
+      </div>
     );
+
+    // return (
+    //   <div className='login'>
+    //   <h1>Login Here!</h1>
+    //   <form onSubmit={handleSubmit}>
+    //     <label>
+    //      Email:
+    //         <input type="text" onChange={(event) => setEmail(event.target.value)} value={email}/>
+    //     </label>
+    //     <label>
+    //      Password:
+    //        <input type="password" onChange={(event) => setPassword(event.target.value)} value={password}/>
+    //     </label>
+    //     <Button sx={{ color: 'white' }} type='submit' variant="outlined">Login</Button>
+    //   </form>
+    //   <h4>
+    //     {error}
+    //   </h4>
+    //   </div>
+    // );
 
 
 }
--- client/src/authentication/Signup.jsx
+++ client/src/authentication/Signup.jsx
@@ -4,6 +4,11 @@ import { signup } from "../features/users/usersSlice";
 import { useSelector } from "react-redux";
 
 
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Box } from "@mui/system";
+
+
 function Signup() {
   const dispatch = useDispatch();
   const errorMessages = useSelector((state) => state.users.errorMessages) 
@@ -30,30 +35,86 @@ function Signup() {
   const renderErrorMessages = errorMessages?.map((e) => <h4>{e}</h4>)
 
   return (
-    <div>
-      <h1>Create Account!</h1>
-      <form onSubmit={createNewUser}>
-        Create Account!
-        <br />
-        <br />
-        <br />
-          <label>First Name: </label>
-          <input type="text" name="first_name" onChange={handleChange} value={user.first_name}/>
-          <br />
-          <label>Last Name: </label>
-          <input type="text" name="last_name" onChange={handleChange} value={user.last_name}/>
-          <br />
-          <label>Email: </label>
-          <input type="text" name="email" onChange={handleChange} value={user.email}/>
-          <br />
-          <label>Password: </label>
-          <input type="password" name="password" onChange={handleChange} value={user.password}/>
-          <br />
-        <button type="submit">Sign up</button>
-      </form>
-      {renderErrorMessages}
-    </div>
-  );
+    <div className="signup">
+    <h1>Create an Account!</h1>
+  <Box
+    onSubmit={createNewUser}
+    component="form"
+    sx={{
+      '& > :not(style)': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <br />
+    <TextField type="text" name="first_name" value={user.first_name} onChange={handleChange} label="First Name" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+    <br />
+    <TextField type="text" name="last_name" value={user.last_name} onChange={handleChange} label="Last Name" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+    <br />
+    <TextField type="email" name="email" value={user.email} onChange={handleChange} label="Email" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+    <br />
+    <TextField type="password" name="password" value={user.password} onChange={handleChange} label="Password" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+    <br />
+    <Button sx={{ color: 'white' }} type='submit' variant="outlined">Sign Up</Button>
+  </Box>
+    <p>
+        {renderErrorMessages}
+    </p>
+  </div>
+)
+
+  // return (
+  //   <div className="signup">
+  //     <h1>Create Account!</h1>
+  //     <form className="rcorners2" onSubmit={createNewUser}>
+  //       <br />
+  //       <br />
+  //       <br />
+  //         <label>First Name: </label>
+  //         <input type="text" name="first_name" onChange={handleChange} value={user.first_name}/>
+  //         <br />
+  //         <label>Last Name: </label>
+  //         <input type="text" name="last_name" onChange={handleChange} value={user.last_name}/>
+  //         <br />
+  //         <label>Email: </label>
+  //         <input type="text" name="email" onChange={handleChange} value={user.email}/>
+  //         <br />
+  //         <label>Password: </label>
+  //         <input type="password" name="password" onChange={handleChange} value={user.password}/>
+  //         <br />
+  //         <Button sx={{ color: 'white' }} type='submit' variant="outlined">Sign Up</Button>
+  //     </form>
+  //     {renderErrorMessages}
+  //   </div>
+  // );
 }
 
 export default Signup
