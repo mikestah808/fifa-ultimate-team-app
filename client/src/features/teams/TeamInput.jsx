e5442a64c2b288fb6d752443ef2fc527723f22d4
Michael Stafford <michaelstafford808@gmail.com>
2023-03-10 12:35:09 -0600

    added material ui box and textfield components to TeamInput component

--- client/src/features/teams/TeamInput.jsx
+++ client/src/features/teams/TeamInput.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createTeam } from "./teamsSlice";
 
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import { render } from "react-dom";
+
 function TeamInput() {
   const teams = useSelector((state) => state.teams.entities) 
   const errorMessages = useSelector((state) => state.teams.errorMessages) 
@@ -30,16 +35,44 @@ function TeamInput() {
 
 
 
+  // return (
+  //   <div>
+  //     <form onSubmit={handleSubmit}>
+  //     <label>Name
+  //     <input onChange={handleChange} value={name}/> 
+  //     </label>
+  //     <button type="submit">Add Team</button>
+  //   </form>
+  //     {renderErrorMessages}
+  //   </div>
+  // )
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-      <label>Name
-      <input onChange={handleChange} value={name}/> 
-      </label>
-      <button type="submit">Add Team</button>
-    </form>
-      {renderErrorMessages}
-    </div>
+    <Box
+        onSubmit={handleSubmit}
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <br />
+        <TextField type="text" id="email" value={name} onChange={handleChange} label="Name" variant="outlined" sx={{
+          input: {
+          color: "white"
+          },
+          label: {
+            color: "white"
+          }
+        }}/>
+        <br />
+        <br />
+        <Button sx={{ color: 'white' }} type='submit' variant="outlined">Submit</Button>
+      </Box>
+        {renderErrorMessages}
+      </div>
   )
 }
 
